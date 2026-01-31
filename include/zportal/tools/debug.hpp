4157a61a29a8822523f05d648ed501ed7bf97cfa
Wiktor Sołtys <wiesiekdx@gmail.com>
2026-01-31 22:56:04 +0100

    Feat(Debug): add `DEBUG_ERRNO` helper

--- include/zportal/tools/debug.hpp
+++ include/zportal/tools/debug.hpp
@@ -1,3 +1,5 @@
+#include <system_error>
+
 #include <cstdio>
 
 #if !defined(NDEBUG)
@@ -5,4 +7,12 @@
         std::fprintf(stderr, "[%s:%d %s] " fmt "\n", __FILE__, __LINE__, __func__ __VA_OPT__(,) __VA_ARGS__)
 #else
 #    define DEBUG_LOG(...) ((void)0)
+#endif
+
+#if !defined(NDEBUG)
+#    define DEBUG_ERRNO(err, context)                                                                                  \
+        std::fprintf(stderr, "[%s:%d %s] %s: %s (%d)\n", __FILE__, __LINE__, __func__, context,                        \
+                     std::system_category().message(err).c_str(), err)
+#else
+#    define DEBUG_ERRNO(...) ((void)0)
 #endif
