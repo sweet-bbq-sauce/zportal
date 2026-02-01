294ef26b271c7c843c66ef30386f20460792ee66
Wiktor Sołtys <wiesiekdx@gmail.com>
2026-02-01 14:14:02 +0100

    Feat(WireHeader): add `WireHeader` class

---
+++ include/zportal/tunnel/wire_header.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <array>
+#include <span>
+
+#include <cstddef>
+#include <cstdint>
+
+namespace zportal {
+
+class WireHeader {
+  public:
+    static constexpr std::uint32_t magic = 0x5A505254;
+    static constexpr std::size_t wire_size = 16;
+
+    void clean() noexcept;
+
+    std::uint32_t get_magic() const noexcept;
+
+    inline std::uint32_t get_flags() const noexcept;
+    void set_flags(std::uint32_t flags) noexcept;
+
+    std::uint32_t get_size() const noexcept;
+    void set_size(std::uint32_t size) noexcept;
+
+    std::uint32_t get_crc() const noexcept;
+    void set_crc(std::uint32_t crc) noexcept;
+
+    std::span<std::byte, wire_size> data() noexcept;
+    std::span<const std::byte, wire_size> data() const noexcept;
+
+  private:
+    std::uint32_t get_u32_(std::size_t offset) const noexcept;
+    void set_u32_(std::size_t offset, std::uint32_t value) noexcept;
+
+    std::array<std::byte, wire_size> data_;
+};
+
+} // namespace zportal
+
+#include "wire_header.inl"
---
+++ tests/tunnel/wire_header_test.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+
+#include <cstdint>
+#include <cstring>
+
+#include <endian.h>
+
+#include <gtest/gtest.h>
+
+#include <zportal/tunnel/wire_header.hpp>
+
+using namespace zportal;
+
+TEST(WireHeader, Decode) {
+    const std::uint32_t magic = WireHeader::magic;
+    const std::uint32_t flags = 5346;
+    const std::uint32_t size = 2345346;
+    const std::uint32_t crc = 54373;
+
+    const std::uint32_t magic_be = ::htobe32(magic);
+    const std::uint32_t flags_be = ::htobe32(flags);
+    const std::uint32_t size_be = ::htobe32(size);
+    const std::uint32_t crc_be = ::htobe32(crc);
+
+    std::vector<std::uint32_t> buffer{magic_be, flags_be, size_be, crc_be};
+
+    if (buffer.size() * sizeof(std::uint32_t) != WireHeader::wire_size)
+        GTEST_SKIP() << "test implementation error";
+
+    WireHeader header;
+    auto header_data = header.data();
+    std::memcpy(header_data.data(), buffer.data(), WireHeader::wire_size);
+
+    EXPECT_EQ(header.get_magic(), magic);
+    EXPECT_EQ(header.get_flags(), flags);
+    EXPECT_EQ(header.get_size(), size);
+    EXPECT_EQ(header.get_crc(), crc);
+}
